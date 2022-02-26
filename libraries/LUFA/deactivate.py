b84a372e7eac0718d020ef87fc762ae5519e13a6
Sanjay Govind <sanjay.govind9@gmail.com>
2022-02-26 16:03:41 +1300

    Test for LUFA integration

---
+++ libraries/LUFA/deactivate.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from activate import deactivate
+"""
+Script to deactivate LUFA for Arduino.
+More info can be found in the activate.py script.
+"""
+
+if __name__ == '__main__':
+    deactivate()
---
+++ libraries/LUFA/install.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from activate import install
+"""
+Script to install LUFA boards for Arduino.
+More info can be found in the activate.py script.
+"""
+
+if __name__ == '__main__':
+    install()
